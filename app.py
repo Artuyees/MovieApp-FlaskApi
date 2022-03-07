c518aefc5902c4f6a3f24f68b885ca4c5dd755ce
Artur Kucinski <artuyees@gmail.com>
2022-03-07 23:01:46 +0100

    added new endpoints

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route("/homepage", methods=["GET"])
-def hello_world():
+def homepage():
     return {"movies": [ {
     "Title": "Nightmare Alley",
 
@@ -82,3 +82,211 @@ def hello_world():
       "https://m.media-amazon.com/images/M/MV5BYTRkOWY1MDktZWM5YS00OTM4LWIxYmUtZDUyMTJiZWQxOTAzXkEyXkFqcGdeQXVyMTEwMTY3NDI@._V1_SX300.jpg",
     "Response": "True",
   }]}
+
+@app.route("/popular", methods=["GET"])
+def popular():
+    return {"movies": [
+  {
+    "Title": "Mafia",
+    
+    "Poster":
+      "https://m.media-amazon.com/images/M/MV5BNTA1MzA2MWQtMTFkYi00ZDFjLTgwNDgtNDU4ZmM4OTlkZGUwXkEyXkFqcGdeQXVyMTIwNzMxNTU2._V1_SX300.jpg",
+    
+    "Response": "True",
+  },
+  {
+    "Title": "Top Gun",
+    
+    "Poster":
+      "https://m.media-amazon.com/images/M/MV5BZjQxYTA3ODItNzgxMy00N2Y2LWJlZGMtMTRlM2JkZjI1ZDhhXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
+    
+    "Response": "True",
+  },
+  {
+    "Title": "Okay",
+   
+    "Poster":
+      "https://m.media-amazon.com/images/M/MV5BMjEwOTQ4NjUwNV5BMl5BanBnXkFtZTcwOTkzNDUyMQ@@._V1_SX300.jpg",
+   
+    "Response": "True",
+  },
+  {
+    "Title": "Filip",
+   
+    "Poster":
+      "https://m.media-amazon.com/images/M/MV5BY2MzODIzMDctNWY4NS00ZWM0LWFiY2QtNzI1YjU0NzEzYWQxXkEyXkFqcGdeQXVyMjI3MDQ5NDY@._V1_SX300.jpg",
+   
+    "Response": "True",
+  },
+  {
+    "Title": "Home Alone",
+    
+    "Poster":
+      "https://m.media-amazon.com/images/M/MV5BMzFkM2YwOTQtYzk2Mi00N2VlLWE3NTItN2YwNDg1YmY0ZDNmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+   
+   
+    "Response": "True",
+  },
+]}
+
+@app.route("/new", methods=["GET"])
+def new():
+    return {"movies": [
+  {
+    "Title": "Nightmare Alley",
+    
+    "Poster":
+      "https://m.media-amazon.com/images/M/MV5BOTI4NDhhNGEtZjQxZC00ZTRmLThmZTctOGJmY2ZlOTc0ZGY0XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg",
+   
+    "Response": "True",
+  },
+  {
+    "Title": "Mafia",
+   
+    "Poster":
+      "https://m.media-amazon.com/images/M/MV5BNTA1MzA2MWQtMTFkYi00ZDFjLTgwNDgtNDU4ZmM4OTlkZGUwXkEyXkFqcGdeQXVyMTIwNzMxNTU2._V1_SX300.jpg",
+    
+    "Response": "True",
+  },
+  {
+    "Title": "Marry Me",
+    
+    "Poster":
+      "https://m.media-amazon.com/images/M/MV5BMjczZjI2M2UtOGMwOS00YWFhLTg0OTYtZDY5ZWIwNjhlOWI2XkEyXkFqcGdeQXVyMTQzNTA5MzYz._V1_SX300.jpg",
+    
+    "Response": "True",
+  },
+  {
+    "Title": "Moonfall",
+    
+    "Poster":
+      "https://m.media-amazon.com/images/M/MV5BZjk0OWZiN2ItNmQ2YS00NTJmLTg0MjItNzM4NzBkMWM1ZTRlXkEyXkFqcGdeQXVyMjMxOTE0ODA@._V1_SX300.jpg",
+    
+    "Response": "True",
+  },
+  {
+    "Title": "Blacklight",
+    
+    "Poster":
+      "https://m.media-amazon.com/images/M/MV5BY2ViYmNhNzEtZDJhNS00YzM4LTkzZWItZDJiNzc5ZDExOWZmXkEyXkFqcGdeQXVyMDA4NzMyOA@@._V1_SX300.jpg",
+    
+    "Response": "True",
+  },
+]}
+
+@app.route("/all", methods=["GET"])
+def all():
+    return {"movies": [
+  {
+    "Title": "Nightmare Alley",
+    
+    "Poster":
+      "https://m.media-amazon.com/images/M/MV5BOTI4NDhhNGEtZjQxZC00ZTRmLThmZTctOGJmY2ZlOTc0ZGY0XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg",
+    
+    "Response": "True",
+  },
+  {
+    "Title": "Spider-Man",
+    
+    "Poster":
+      "https://m.media-amazon.com/images/M/MV5BZDEyN2NhMjgtMjdhNi00MmNlLWE5YTgtZGE4MzNjMTRlMGEwXkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_SX300.jpg",
+    
+    "Response": "True",
+  },
+  {
+    "Title": "Avatar",
+    
+    "Poster":
+      "https://m.media-amazon.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+   
+    "Response": "True",
+  },
+  {
+    "Title": "Titanic",
+    
+    "Poster":
+      "https://m.media-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg",
+    
+    "Response": "True",
+  },
+  {
+    "Title": "Pierwszy Polak na Marsie",
+    
+    "Poster":
+      "https://m.media-amazon.com/images/M/MV5BYTRkOWY1MDktZWM5YS00OTM4LWIxYmUtZDUyMTJiZWQxOTAzXkEyXkFqcGdeQXVyMTEwMTY3NDI@._V1_SX300.jpg",
+    
+    "Response": "True",
+  },
+  {
+    "Title": "Mission: Impossible",
+    
+    "Poster":
+      "https://m.media-amazon.com/images/M/MV5BMTc3NjI2MjU0Nl5BMl5BanBnXkFtZTgwNDk3ODYxMTE@._V1_SX300.jpg",
+   
+    "Response": "True",
+  },
+  {
+    "Title": "The Godfather",
+    
+    "Poster":
+      "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+    
+    "Response": "True",
+  },
+  {
+    "Title": "Toy Story",
+    
+    "Poster":
+      "https://m.media-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_SX300.jpg",
+    
+    "Response": "True",
+  },
+  {
+    "Title": "Mafia",
+    
+    "Poster":
+      "https://m.media-amazon.com/images/M/MV5BNTA1MzA2MWQtMTFkYi00ZDFjLTgwNDgtNDU4ZmM4OTlkZGUwXkEyXkFqcGdeQXVyMTIwNzMxNTU2._V1_SX300.jpg",
+    
+    "Response": "True",
+  },
+  {
+    "Title": "Top Gun",
+    
+    "Poster":
+      "https://m.media-amazon.com/images/M/MV5BZjQxYTA3ODItNzgxMy00N2Y2LWJlZGMtMTRlM2JkZjI1ZDhhXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
+    
+    "Response": "True",
+  },
+  {
+    "Title": "Okay",
+    
+    "Poster":
+      "https://m.media-amazon.com/images/M/MV5BMjEwOTQ4NjUwNV5BMl5BanBnXkFtZTcwOTkzNDUyMQ@@._V1_SX300.jpg",
+    
+    "Response": "True",
+  },
+  {
+    "Title": "Filip",
+    
+    "Poster":
+      "https://m.media-amazon.com/images/M/MV5BY2MzODIzMDctNWY4NS00ZWM0LWFiY2QtNzI1YjU0NzEzYWQxXkEyXkFqcGdeQXVyMjI3MDQ5NDY@._V1_SX300.jpg",
+    
+    "Response": "True",
+  },
+  {
+    "Title": "Home Alone",
+    
+    "Poster":
+      "https://m.media-amazon.com/images/M/MV5BMzFkM2YwOTQtYzk2Mi00N2VlLWE3NTItN2YwNDg1YmY0ZDNmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+    
+    "Response": "True",
+  },
+  {
+    "Title": "Welcome",
+    
+    "Poster":
+      "https://m.media-amazon.com/images/M/MV5BZjcyOTViMzUtOWQ5Yy00ZTVmLWJmYzctN2U2OGVlN2ZjNTA0XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg",
+    "Response": "True",
+  },
+]
+}
