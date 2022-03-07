ddd246f0500326d9a5d473da083c3f9b355f20f0
Artur Kucinski <artuyees@gmail.com>
2022-03-07 21:57:10 +0100

    fixed cors bug

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 # data comes from http://www.omdbapi.com made by Brian Fritz. All content licensed under CC BY-NC 4.0.
 
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/homepage", methods=["GET"])
 def hello_world():
